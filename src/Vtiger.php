ef398bdff31d3e4cc48c698c22e9be443fae9e14
Puspendra Singh <nsrajpoot87@gmail.com>
2025-02-04 12:34:54 +0530

    Update Vtiger.php

--- src/Vtiger.php
+++ src/Vtiger.php
@@ -22,23 +22,22 @@ class Vtiger
     protected string $persistConnection;
     protected Client $guzzleClient;
     protected int    $maxRetries;
-
+    
     /**
      * Vtiger constructor.
      */
     public function __construct()
     {
         // Obtain the connection information.
-        $this->url               = config( 'vtiger.url' );
-        $this->username          = config( 'vtiger.username' );
-        $this->accessKey         = config( 'vtiger.accesskey' );
-        $this->persistConnection = config( 'vtiger.persistconnection' );
-        $this->maxRetries        = config( 'vtiger.max_retries' );
-
+        $this->url               = config( 'vtiger.vtiger_url' );
+        $this->username          = config( 'vtiger.vtiger_username' );
+        $this->accessKey         = config( 'vtiger.vtiger_key' );
+        $this->persistConnection = config( 'vtiger.vtiger_persistent' );
+        $this->maxRetries        = config( 'vtiger.vtiger_retries' );
         // Initiate a new GuzzleHttp Client.
         $this->guzzleClient = new Client( ['http_errors' => false, 'verify' => false] );
     }
-
+    
     /**
      * Override configured connection details.
      *
@@ -53,10 +52,10 @@ class Vtiger
         $this->url       = $url;
         $this->username  = $username;
         $this->accessKey = $accessKey;
-
+        
         return $this;
     }
-
+    
     /**
      * Get the session id for a login either from a stored session id or fresh from the API
      *
@@ -67,18 +66,34 @@ class Vtiger
     {
         // Get the sessionData from the cache
         $sessionData = json_decode( Cache::get( 'vtiger_laravel' ) );
-
+        
         if( !isset( $sessionData, $sessionData->expireTime, $sessionData->token ) || $sessionData->expireTime < time() || empty( $sessionData->token ) ) {
             $sessionData = $this->storeSession();
         }
-
+        
         if( isset( $sessionData->sessionid ) ) {
             return $sessionData->sessionid;
         }
-
+        
         return $this->login( $sessionData );
     }
-
+    
+    /**
+     * Get the session id for a login either from a stored session id or fresh from the API
+     *
+     * @return string|null
+     * @throws GuzzleException
+     */
+    public function userId()
+    {
+        // Get the sessionData from the cache
+        $sessionData = json_decode( Cache::get( 'vtiger_laravel' ) );
+        if( isset( $sessionData->userid ) ) {
+            return $sessionData->userid;
+        }
+        return NULL;
+    }
+    
     /**
      * Login to the VTiger API to get a new session
      *
@@ -91,12 +106,12 @@ class Vtiger
     {
         $sessionId = NULL;
         $token     = $sessionData->token;
-
+        
         // Create unique key using combination of challenge token and access key
         $generatedKey = md5( $token . $this->accessKey );
-
+        
         $tryCounter = 1;
-
+        
         do {
             // login using username and accesskey
             $response = $this->guzzleClient->request( 'POST', $this->url, [
@@ -106,16 +121,16 @@ class Vtiger
                     'accessKey' => $generatedKey,
                 ],
             ] );
-
+            
             // decode the response
             $loginResult = $this->_processResponse( $response );
             $tryCounter++;
         } while( !isset( $loginResult->success ) && $tryCounter <= $this->maxRetries );
-
+        
         if( $tryCounter >= $this->maxRetries ) {
             throw new Exception( 'Could not complete login request within ' . $this->maxRetries . ' tries' );
         }
-
+        
         // Check if the response is invalid, or not successful.
         if( $response->getStatusCode() !== 200 || !$loginResult->success ) {
             if( $loginResult->error->code == 'INVALID_USER_CREDENTIALS' || $loginResult->error->code == 'INVALID_SESSIONID' ) {
@@ -128,20 +143,22 @@ class Vtiger
         else {
             // Response is valid.
             $sessionId = $loginResult->result->sessionName;
-
+            $userId = $loginResult->result->userId;
+            
             if( Cache::has( 'vtiger_laravel' ) ) {
                 $json            = json_decode( Cache::pull( 'vtiger_laravel' ) );
                 $json->sessionid = $sessionId;
+                $json->userid = $userId;
                 Cache::forever( 'vtiger_laravel', json_encode( $json ) );
             }
             else {
                 throw new Exception( 'Laravel cache key "vtiger_laravel" does not exist.' );
             }
         }
-
+        
         return $sessionId;
     }
-
+    
     /**
      * Store a new session if needed
      *
@@ -151,13 +168,13 @@ class Vtiger
     protected function storeSession(): object
     {
         $updated = $this->getToken();
-
+        
         $output = (object)$updated;
         Cache::forever( 'vtiger_laravel', json_encode( $output ) );
-
+        
         return $output;
     }
-
+    
     /**
      * Get a new access token from the VTiger API
      *
@@ -175,23 +192,23 @@ class Vtiger
                     'username'  => $this->username,
                 ],
             ] );
-
+            
             $tryCounter++;
         } while( !isset( $this->_processResponse( $response )->success ) && $tryCounter <= $this->maxRetries );
-
+        
         if( $tryCounter >= $this->maxRetries ) {
             throw new Exception( 'Could not complete get token request within ' . $this->maxRetries . ' tries' );
         }
-
+        
         // decode the response
         $challenge = $this->_processResponse( $response );
-
+        
         return [
             'token'      => $challenge->result->token,
             'expireTime' => $challenge->result->expireTime,
         ];
     }
-
+    
     /**
      * Logout from the VTiger API
      *
@@ -205,7 +222,7 @@ class Vtiger
         if( $this->persistConnection ) {
             return true;
         }
-
+        
         // send a request to close current connection
         $response = $this->guzzleClient->request(
             'POST',
@@ -216,11 +233,11 @@ class Vtiger
                     'sessionName' => $sessionId,
                 ],
             ]
-        );
-
+            );
+        
         return $this->_processResponse( $response );
     }
-
+    
     /**
      * Query the VTiger API with the given query string
      *
@@ -232,7 +249,7 @@ class Vtiger
     public function query( string $query )
     {
         $sessionId = $this->sessionId();
-
+        
         // send a request using a database query to get back any matching records
         $response = $this->guzzleClient->request( 'GET', $this->url, [
             'query' => [
@@ -241,17 +258,17 @@ class Vtiger
                 'query'       => $query,
             ],
         ] );
-
+        
         $this->close( $sessionId );
-
+        
         return $this->_processResponse( $response );
     }
-
+    
     public function search( $query, $quote = true )
     {
         $bindings    = $query->getBindings();
         $queryString = $query->toSQL();
-
+        
         foreach( $bindings as $binding ) {
             if( $quote ) {
                 $queryString = preg_replace( '/\?/', DB::connection()->getPdo()->quote( $binding ), $queryString, 1 );
@@ -260,7 +277,7 @@ class Vtiger
                 $queryString = preg_replace( '/\?/', $binding, $queryString, 1 );
             }
         }
-
+        
         // In the event there is an offset, append it to the front of the limit
         // Vtiger does not support the offset keyword
         $matchOffset = [];
@@ -270,17 +287,17 @@ class Vtiger
             $queryString = preg_replace( '/(\s[l][i][m][i][t]) (\d*)/', '', $queryString );
             $queryString = $queryString . ' limit ' . $matchOffset[2] . ',' . $matchLimit[2];
         }
-
+        
         // Remove the backticks and add semicolon
         $queryString = str_replace( '`', '', $queryString ) . ';';
-
+        
         return $this->query( $queryString );
     }
-
+    
     public function listTypes()
     {
         $sessionId = $this->sessionId();
-
+        
         // Lookup the data
         // send a request to retrieve all list types
         $response = $this->guzzleClient->request( 'GET', $this->url, [
@@ -289,25 +306,25 @@ class Vtiger
                 'sessionName' => $sessionId,
             ],
         ] );
-
+        
         $this->close( $sessionId );
-
+        
         return $this->_processResponse( $response );
     }
-
+    
     public function lookup( $dataType, $value, $module, array $columns )
     {
         $sessionId = $this->sessionId();
-
+        
         // Update columns into the proper format
         $columnsText = '';
         foreach( $columns as $column ) {
             $columnsText .= '"' . $column . '",';
         }
-
+        
         // Trim the last comma from the string
         $columnsText = substr( $columnsText, 0, (strlen( $columnsText ) - 1) );
-
+        
         // Lookup the data
         // send a request to retrieve a record
         $response = $this->guzzleClient->request( 'GET', $this->url, [
@@ -319,12 +336,12 @@ class Vtiger
                 'searchIn'    => '{"' . $module . '":[' . $columnsText . ']}',
             ],
         ] );
-
+        
         $this->close( $sessionId );
-
+        
         return $this->_processResponse( $response );
     }
-
+    
     /**
      * Retrieve a record from the VTiger API
      * Format of id must be {module_code}x{item_id}, e.g 4x12
@@ -337,7 +354,7 @@ class Vtiger
     public function retrieve( string $id )
     {
         $sessionId = $this->sessionId();
-
+        
         // send a request to retrieve a record
         $response = $this->guzzleClient->request( 'GET', $this->url, [
             'query' => [
@@ -346,12 +363,12 @@ class Vtiger
                 'id'          => $id,
             ],
         ] );
-
+        
         $this->close( $sessionId );
-
+        
         return $this->_processResponse( $response );
     }
-
+    
     /**
      * Create a new entry in the VTiger API
      *
@@ -366,7 +383,7 @@ class Vtiger
     public function create( $elem, array $data )
     {
         $sessionId = $this->sessionId();
-
+        
         // send a request to create a record
         $response = $this->guzzleClient->request( 'POST', $this->url, [
             'form_params' => [
@@ -376,12 +393,12 @@ class Vtiger
                 'elementType' => $elem,
             ],
         ] );
-
+        
         $this->close( $sessionId );
-
+        
         return $this->_processResponse( $response );
     }
-
+    
     /**
      * Update an entry in the database from the given object
      *
@@ -395,7 +412,7 @@ class Vtiger
     public function update( $object )
     {
         $sessionId = $this->sessionId();
-
+        
         // send a request to update a record
         $response = $this->guzzleClient->request( 'POST', $this->url, [
             'form_params' => [
@@ -404,12 +421,12 @@ class Vtiger
                 'element'     => json_encode( $object ),
             ],
         ] );
-
+        
         $this->close( $sessionId );
-
+        
         return $this->_processResponse( $response );
     }
-
+    
     /**
      * Delete from the database using the given id
      * Format of id must be {moudler_code}x{item_id}, e.g 4x12
@@ -422,7 +439,7 @@ class Vtiger
     public function delete( string $id )
     {
         $sessionId = $this->sessionId();
-
+        
         // send a request to delete a record
         $response = $this->guzzleClient->request( 'POST', $this->url, [
             'form_params' => [
@@ -431,12 +448,12 @@ class Vtiger
                 'id'          => $id,
             ],
         ] );
-
+        
         $this->close( $sessionId );
-
+        
         return $this->_processResponse( $response );
     }
-
+    
     /**
      * Describe an element from the vTiger API from the given element name
      *
@@ -448,7 +465,7 @@ class Vtiger
     public function describe( string $elementType )
     {
         $sessionId = $this->sessionId();
-
+        
         // send a request to describe a module (which returns a list of available fields) for a Vtiger module
         $response = $this->guzzleClient->request(
             'GET',
@@ -460,13 +477,13 @@ class Vtiger
                     'elementType' => $elementType,
                 ],
             ]
-        );
-
+            );
+        
         $this->close( $sessionId );
-
+        
         return $this->_processResponse( $response );
     }
-
+    
     /**
      * @param $response
      *
@@ -487,7 +504,62 @@ class Vtiger
         else {
             $data = $response;
         }
-
+        
         return $data;
+    }
+    
+    public function addRelated($sourceRecordId,$relatedRecordId)
+    {
+        $sessionId = $this->sessionId();
+        
+        // Lookup the data
+        // send a request to retrieve all list types
+        $response = $this->guzzleClient->request( 'POST', $this->url, [
+            'form_params' => [
+                'operation'   => 'add_related',
+                'sessionName' => $sessionId,
+                'sourceRecordId' => $sourceRecordId,
+                'relatedRecordId' => $relatedRecordId,
+            ],
+        ] );
+        
+        $this->close( $sessionId );
+        
+        return $this->_processResponse( $response );
+    }
+    
+    /**
+     *
+     * @param string $crmLoginUserId
+     * @param string $casinoUserId
+     * @param string $gameId
+     * @param string $shopId
+     * @param string $userIPAdds
+     * @param string $spinSymbols json data
+     * @param array $gameState
+     */
+    public function gameHistory(string $crmLoginUserId,string $casinoUserId,string $gameId,string $shopId,string $userIPAdds,string $spinSymbols,$gameState=array()) {
+        $spinSymbolsObj = json_decode($spinSymbols);
+        $responseEvent = $spinSymbolsObj->responseEvent;
+        $responseType = $spinSymbolsObj->responseType;
+        $openBalance = $spinSymbolsObj->serverResponse->Balance;
+        $afterBalance = $spinSymbolsObj->serverResponse->afterBalance;
+        $slotBetVolumn = $gameState['bet'];
+        $slotTotalWin = $spinSymbolsObj->serverResponse->totalWin;
+        $createData = array(
+            "trading_id" => $crmLoginUserId,
+            "login" => $gameState['game'],
+            "order_id" => $shopId,
+            "symbol" => $responseEvent,
+            "type" => $responseType,
+            "comment"=>$userIPAdds,
+            "game_logs"=>$spinSymbols,
+            "volume" => $slotBetVolumn,"assigned_user_id"=>$this->userId(),"source"=>"Casino","open_price"=>$openBalance,"close_price"=>$afterBalance,"profit"=>$slotTotalWin
+        );
+        $res = $this->create("Tradinghistory", $createData);
+        if($res->success){
+            $tradehistoryId = $res->result->id;
+            $resR = $this->addRelated($crmLoginUserId,$tradehistoryId);
+        }
     }
 }
